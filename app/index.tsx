fbd68841b9a62ff84c78558803c38aae2aa60c79
bammyu <mbjun3630@naver.com>
2025-04-20 13:27:29 +0900

    feat: add nativewind library

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
-import { Text, View } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import { Switch, Text, View } from 'react-native';
 
 export default function Index() {
+  const { colorScheme, setColorScheme } = useColorScheme();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white dark:bg-black">
+      <Text className="text-black dark:text-white">Edit app/index.tsx to edit this screen.</Text>
+      <Switch
+        value={colorScheme === 'dark'}
+        onValueChange={() => setColorScheme(colorScheme === 'light' ? 'dark' : 'light')}
+      />
     </View>
   );
 }
