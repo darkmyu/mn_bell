cec9f97fa08ec0e763b3a1a9ecca0b97038a2057
bammyu <mbjun3630@naver.com>
2025-07-11 11:29:55 +0900

    feat: add common button component

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -1,9 +1,11 @@
 import AlarmRepeatSelector from '@/components/alarm/AlarmRepeatSelector';
 import AlarmSummary from '@/components/alarm/AlarmSummary';
 import AlarmTimeSelector from '@/components/alarm/AlarmTimeSelector';
+import Button from '@/components/common/Button';
 import { useAlarmCreateForm } from '@/hooks/forms/alarm';
 import React from 'react';
 import { FormProvider } from 'react-hook-form';
+import { View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { StyleSheet } from 'react-native-unistyles';
 
@@ -12,19 +14,37 @@ export default function FormScreen() {
 
   return (
     <FormProvider {...methods}>
-      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.container}>
-        <AlarmSummary />
-        <AlarmTimeSelector />
-        <AlarmRepeatSelector />
-      </ScrollView>
+      <View style={styles.container}>
+        <ScrollView contentContainerStyle={styles.form} showsVerticalScrollIndicator={false}>
+          <AlarmSummary />
+          <AlarmTimeSelector />
+          <AlarmRepeatSelector />
+        </ScrollView>
+        <View style={styles.floating}>
+          <Button title="저장하기" onPress={() => {}} />
+        </View>
+      </View>
     </FormProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  form: {
     gap: 24,
-    marginVertical: 32,
+    marginTop: 32,
     marginHorizontal: 16,
+    paddingBottom: 160,
+  },
+  floating: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    paddingTop: 32,
+    paddingBottom: 48,
+    paddingHorizontal: 16,
   },
 });
--- src/components/alarm/AlarmFloating.tsx
+++ src/components/alarm/AlarmFloating.tsx
@@ -1,8 +1,9 @@
 import { ROUTE_FORM_SCREEN } from '@/constants/route';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import { useRouter } from 'expo-router';
-import { Pressable, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { StyleSheet, useUnistyles } from 'react-native-unistyles';
+import Button from '../common/Button';
 
 function AlarmFloating() {
   const router = useRouter();
@@ -15,10 +16,11 @@ function AlarmFloating() {
   return (
     <View style={styles.container}>
       <View style={styles.backdrop}>
-        <Pressable style={({ pressed }) => styles.floating(pressed)} onPress={handlePressed}>
-          <FontAwesome5 name="plus" size={16} color={theme.colors.white} />
-          <Text style={styles.text}>빵이 일정 생성</Text>
-        </Pressable>
+        <Button
+          title="빵이 일정 추가"
+          onPress={handlePressed}
+          startIcon={<FontAwesome5 name="plus" size={14} color={theme.colors.white} />}
+        />
       </View>
     </View>
   );
@@ -39,21 +41,6 @@ const styles = StyleSheet.create((theme) => ({
     paddingHorizontal: 16,
     backgroundColor: theme.colors.background,
     boxShadow: `0 -10px 10px ${theme.colors.background}`,
-  },
-  floating: (pressed: boolean) => ({
-    height: 48,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 8,
-    borderRadius: 12,
-    backgroundColor: theme.colors.secondary,
-    opacity: pressed ? 0.75 : 1,
-  }),
-  text: {
-    fontSize: 16,
-    fontFamily: theme.fonts.bold,
-    color: theme.colors.white,
   },
 }));
 
